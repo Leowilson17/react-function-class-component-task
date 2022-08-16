20065adf3b518a529dfa3ede17da7e7d27c6f0db
leowilson <leobca2018@gmail.com>
2022-08-16 16:56:35 +0530

    push-project-code.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home/home';
+import Slider from "./pages/Slider/slider";
+import Box from "./pages/boxes/box";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home/>  
+      <Slider/>
+      <Box />
     </div>
   );
 }
+
+
+
 
 export default App;
---
+++ src/pages/Home/home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <div className="black">
+        <div className="container">
+          <p className="para">Better digital experience with Presento</p>
+          <p className="para-2">
+            We are team of talented designers making websites with Bootstrap
+          </p>
+          <button type="button" className="btn-1">
+            get started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
